294406d3683c11213fd8a5923b8d644d31fd8b23
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-06-06 20:21:59 +0000

    Localisation updates from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki@115484 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- ArticleToCategory2.i18n.php
+++ ArticleToCategory2.i18n.php
@@ -404,17 +404,18 @@ $messages['it'] = array(
 
 /** Japanese (日本語)
  * @author Schu
+ * @author Shirayuki
  * @author Yanajin66
  * @author 青子守歌
  */
 $messages['ja'] = array(
-	'articletocategory2-desc' => '各カテゴリーのページに、新しいページを直接そのカテゴリーへ追加して作成することのできる入力ボックスを追加します。',
-	'articletocategory2-create-article-under-category-text' => 'このカテゴリーにページを追加',
+	'articletocategory2-desc' => '各カテゴリのページに、新しいページをそのカテゴリ内に直接作成できる入力ボックスを追加する',
+	'articletocategory2-create-article-under-category-text' => 'このカテゴリにページを追加',
 	'articletocategory2-create-article-under-category-button' => '作成',
-	'articletocategory2-create-category-under-category-text' => 'このカテゴリーにカテゴリーを追加',
+	'articletocategory2-create-category-under-category-text' => 'このカテゴリにカテゴリを追加',
 	'articletocategory2-create-category-under-category-button' => '作成',
-	'right-ArticleToCategory2' => 'カテゴリーにページを追加',
-	'right-ArticleToCategory2AddCat' => 'カテゴリーにカテゴリーを追加',
+	'right-ArticleToCategory2' => 'カテゴリにページを追加',
+	'right-ArticleToCategory2AddCat' => 'カテゴリにカテゴリを追加',
 );
 
 /** Khmer (ភាសាខ្មែរ)
