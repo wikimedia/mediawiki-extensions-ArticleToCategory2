aeabc53887096fcc2951dbb183e948808c2cbad2
Jack Phoenix <ashley@uncyclomedia.co>
2021-05-15 21:12:34 +0300

    Cleanup & rework
    * Use standard MediaWiki functions instead of superglobals to reduce reviewer anxiety & to make the code more readable
    * Use TemplateParser (in core since MW 1.25) to build the HTML
    * Split JS from the HTML into its own thing, clean it up and load it via ResourceLoader
    ** Since the problematic comment is now gone, this also fixes T174373
    * Nobody likes magic numbers or remembering them, so use NS_MEDIAWIKI constant instead of the magic number "8" for Title::newFromText()
    * More RequestContext in general
    * Remove silly "echo" statement
    * Adjust documentation to match reality
    * Rename hooked functions to "on" + core hook name for clarity
    * Bump version number
    
    Change-Id: I5077a3e5addd6715812e821ff6169e9a4b30dfb1

---
+++ resources/ext.articletocategory2.js
@@ -0,0 +1,48 @@
+( function () {
+
+	function clearText( theField ) {
+		if ( theField.defaultValue === theField.value ) {
+			theField.value = '';
+		}
+	}
+
+	function addText( theField ) {
+		if ( theField.value === '' ) {
+			theField.value = theField.defaultValue;
+		}
+	}
+
+	function addTextTitle( theField ) {
+		if ( theField.value === '' ) {
+			theField.value = theField.defaultValue;
+		} else {
+			theField.value = mw.config.get( 'wgFormattedNamespaces' )[ 14 ] + ':' + theField.value;
+		}
+	}
+
+	function isEmptyX( form ) {
+		if ( form.title.value === '' || form.title.value === form.title.defaultValue ) {
+			return false;
+		}
+		return true;
+	}
+
+	$( function () {
+		$( 'form[name="createbox"]' ).submit( function () {
+			isEmptyX( this );
+		} );
+
+		$( '.createboxInput' ).focus( function () {
+			clearText( this );
+		} );
+
+		$( '.add-page-btn' ).blur( function () {
+			addText( this );
+		} );
+
+		$( '.add-cat-btn' ).blur( function () {
+			addTextTitle( this );
+		} );
+	} );
+
+}() );
